73258167b1687ec04f5f98a42f2fdf9bed52e9cf
landermo <lmontgomery@boomtownroi.com>
2017-01-16 19:05:06 -0500

    Destination counts Working on bonuses

--- data_parser.rb
+++ data_parser.rb
@@ -10,12 +10,25 @@
 # Leela - pilots everywhere else because she is the only responsible one
 
 
-
-
-
 require 'csv'
-
 revenue = 0
+bonus_per_person = {'Fry' => 0, 'Amy' => 0, 'Bender' => 0, 'Leela' => 0}
+destination_counts = {'Earth' => 0, 'Moon' => 0, 'Mars' => 0, 'Uranus' => 0, 'Jupiter' => 0, 'Pluto' => 0, 'Saturn' => 0, 'Mercury' => 0}
+
+def pilot_for_planet(planet)
+ if planet == 'Earth'
+    'Fry'
+   if planet == 'Uranus'
+       'Bender'
+       if planet == 'Mars'
+         'Amy'
+         else
+          'Leela'
+         end
+     end
+   end
+ end
+
 
 CSV.foreach('/Users/lmontgomery/RubymineProjects/csv-reporting/planet_express_logs.csv', headers: true) do |row|
   # puts row.inspect
@@ -25,10 +38,17 @@ CSV.foreach('/Users/lmontgomery/RubymineProjects/csv-reporting/planet_express_lo
   puts "Money this week: #{money_this_week} Bonus: #{bonus}"
   revenue += money_this_week
 
+
+
+  planet = row['Destination']
+  current_count = destination_counts[planet]
+  new_count = current_count + 1
+  destination_counts[planet] = new_count
+
 end
-
+puts ' '
 puts "Total revenue is #{revenue}"
-
+puts "Destination counts #{destination_counts}"
 
 
 
