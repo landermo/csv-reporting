64548aad1ab401b0dd0a273f1da0b13cdc3c11b9
landermo <lmontgomery@boomtownroi.com>
2017-01-16 21:47:39 -0500

    Trying to get bonus and destination assigned to pilot

--- data_parser.rb
+++ data_parser.rb
@@ -2,7 +2,7 @@
 # How much of a bonus did each employee get? (bonuses are paid to employees who pilot the Planet Express)
 # How many trips did each employee pilot?
 # How much money did we make broken down by planet? ie.. how much did we make shipping to Earth? Mars? Saturn? etc.
-#     Different employees have favorite destinations they always pilot to
+# Different employees have favorite destinations they always pilot to
 #
 # Fry - pilots to Earth (because he isn't allowed into space)
 # Amy - pilots to Mars (so she can visit her family)
@@ -11,23 +11,29 @@
 
 
 require 'csv'
+require 'tempfile'
+
+temp = Tempfile.new("csv")
 revenue = 0
+bonus = 0
 bonus_per_person = {'Fry' => 0, 'Amy' => 0, 'Bender' => 0, 'Leela' => 0}
 destination_counts = {'Earth' => 0, 'Moon' => 0, 'Mars' => 0, 'Uranus' => 0, 'Jupiter' => 0, 'Pluto' => 0, 'Saturn' => 0, 'Mercury' => 0}
 
+
 def pilot_for_planet(planet)
- if planet == 'Earth'
-    'Fry'
-   if planet == 'Uranus'
-       'Bender'
-       if planet == 'Mars'
-         'Amy'
-         else
-          'Leela'
-         end
-     end
+  if planet == 'Earth'
+  pilot = 'Fry'
+  if planet == 'Mars'
+  pilot = 'Amy'
+  if planet == 'Uranus'
+  pilot = 'Bender'
+    else pilot = 'Leela'
+    end
+    end
    end
- end
+  end
+
+
 
 
 CSV.foreach('/Users/lmontgomery/RubymineProjects/csv-reporting/planet_express_logs.csv', headers: true) do |row|
@@ -35,23 +41,22 @@ CSV.foreach('/Users/lmontgomery/RubymineProjects/csv-reporting/planet_express_lo
 
   money_this_week = row['Crates'].to_f * row['Money'].to_f
   bonus = money_this_week * 0.05
-  puts "Money this week: #{money_this_week} Bonus: #{bonus}"
+
+  puts "Money this week: #{row['Destination']} #{money_this_week}: Bonus: #{bonus}"
   revenue += money_this_week
-
-
 
   planet = row['Destination']
   current_count = destination_counts[planet]
   new_count = current_count + 1
   destination_counts[planet] = new_count
-
 end
+
+# CSV.open(temp, "w") do |temp_csv|
+#   CSV.foreach('/Users/lmontgomery/RubymineProjects/csv-reporting/planet_express_logs.csv') do |orig|
+#     temp_csv << orig + [revenue] + [bonus]
+#   end
+# end
 puts ' '
 puts "Total revenue is #{revenue}"
 puts "Destination counts #{destination_counts}"
-
-
-
-
-
 
